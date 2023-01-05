27ab06501f2900b56ea399a45bde3be9fdb8086f
stephanesdr <stadlerstephane@gmail.com>
2023-01-05 11:01:14 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  content: ["./src/*.html", "./src/*.{js,jsx,ts,tsx,vue}"],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
